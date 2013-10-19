f24c3c0c9b384f2e20fb435f065a83c890e1d1ff
seanholden <seanholden1@gmail.com>
2013-10-19 15:45:48 +0100

    completed GameCoverFinder lib class. Tests need writing for peace of mind.

--- lib/support/game_cover_finder.rb
+++ lib/support/game_cover_finder.rb
@@ -7,50 +7,65 @@ class GameCoverFinder
   GAME_LIST_URL = "http://thegamesdb.net/api/GetGamesList.php?name="
   GAME_URL = "http://thegamesdb.net/api/GetGame.php?id="
 
-  def display_images(query)
-    game_ids = get_game_ids(query)
-    url = GAME_URL + game_ids[0]
-    data = HTTParty.get(url).parsed_response['Data']
-    base_img_url = data['baseImgUrl']
-    title = data['Game']['GameTitle']
-    puts get_boxart(data['Game']['Images']['boxart'], base_img_url)
-    puts get_fanart(data['Game']['Images']['fanart'], base_img_url)
-    return '---'
+  def display_images(query, limit=50)
+    game_images = []
+    game_ids = get_game_ids(query, limit)
+    game_ids.each do |game_id|
+      url = GAME_URL + game_id
+      data = HTTParty.get(url).parsed_response['Data']
+      base_img_url = data['baseImgUrl']
+      title = data['Game']['GameTitle']
+      boxart = get_boxart(data['Game']['Images']['boxart'], base_img_url) || []
+      fanart = get_fanart(data['Game']['Images']['fanart'], base_img_url) || []
+      game_images << {title:title, images:boxart+fanart}
+    end
+
+    return game_images.to_json
   end
 
-  def get_game_ids(query)
+  private
+
+  def get_game_ids(query, limit)
     url = GAME_LIST_URL + query
-    games_list = HTTParty.get(url).parsed_response['Data']['Game']
+    games_list = HTTParty.get(url).parsed_response['Data']['Game'] || []
     game_ids = []
-    games_list.each do |game|
-      game_ids << game['id']
+    if games_list.kind_of?(Array)
+      games_list.first(limit).each { |game| game_ids << game['id'] }
+    else
+      game_ids << games_list['id']
     end
+
     return game_ids
   end
 
   def get_boxart(boxart_images, base_img_url)
-    boxart_front, boxart_back = nil
-    boxart_images.each do |boxart|
-      if boxart['side'] == 'front'
-        boxart_front = boxart['__content__']
-      elsif boxart['side'] == 'back'
-        boxart_back = boxart['__content__']
-      end
+    boxart = []
+    if boxart_images.kind_of?(Array)
+      boxart_images.each { |boxart_image| boxart << boxart_image['__content__'] }
+    elsif boxart_images.kind_of?(Hash)
+      boxart << boxart_images['__content__']
     end
-    return [boxart_front, boxart_back].map { |url| base_img_url+url }
+
+    return boxart.map { |url| base_img_url+url }
   end
 
-  def get_fanart(fanart_array, base_img_url)
-    image_url = nil
+  def get_fanart(fanart_images, base_img_url)
+    unless fanart_images.nil?
+      fanart = store_fanart_images(fanart_images, base_img_url)
+    end
+
+    return fanart
+  end
+
+  def store_fanart_images(fanart_images, base_img_url)
     image_urls = []
-    fanart_array.each do |fanart|
-      image_urls << fanart['original']['__content__']
+    if fanart_images.kind_of?(Array)
+      fanart_images.each { |fanart| image_urls << fanart['original']['__content__'] }
+      image_urls = image_urls.map { |url| base_img_url+url }
+    elsif fanart_images.kind_of?(Hash)
+      image_urls << base_img_url+fanart_images['original']['__content__']
     end
-    return image_urls.map { |url| base_img_url+url } 
+
+    return image_urls
   end
-
-
-
 end
-
-
