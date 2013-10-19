797dc47e2c255f052fb6df2dd74c7c1fa2246549
Karl Turner <karl.turner5@gmail.com>
2013-10-19 18:18:59 +0100

    add helpers for lib vars

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def movie_finder
+    @movie_finder ||= MoviePosterFinder.new(movie_api_key)
+  end
+
+  def games_finder
+    @games_finder ||= GameCoverFinder.new
+
+  def movie_api_key
+    ENV['MOVIE_API_KEY']
+  end
 end
