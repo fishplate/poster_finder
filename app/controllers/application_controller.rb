0c69dc166c2a510d6bae047ae5d8b7b45c53c294
Karl Turner <karl.turner5@gmail.com>
2013-10-19 19:18:28 +0100

    results controller to return the movie stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,20 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def movie_client
+    @movie_client ||= MoviePosterFinder.new(movie_api_key)
+  end
+
+  def movie_api_key
+    ENV['MOVIE_API_KEY']
+  end
+
+  def values_missing?(query_string, search_string)
+    empty_string?(query_string) || empty_string?(search_string)
+  end
+
+  def empty_string?(query_string)
+    query_string.nil? || query_string.to_s.empty?
+  end
 end
