4e32e0c488b61dfc01ae50a3178148a15bb89146
Karl Turner <karl.turner5@gmail.com>
2013-10-20 19:49:43 +0100

    updated to use correct secret_token

--- config/deploy.rb
+++ config/deploy.rb
@@ -75,6 +75,7 @@ after 'deploy:update_code' do
   # Setup Configuration
   run "cp #{shared_path}/config/database.yml #{release_path}/config/database.yml"
   run "cp #{shared_path}/config/config.yml #{release_path}/config/config.yml"
+  run "cp #{shared_path}/config/initializers/secret_token.rb #{release_path}/config/initializers/secret_token.rb"
  
   # Compile Assets
   run "cd #{release_path}; RAILS_ENV=production bundle exec rake assets:precompile"
